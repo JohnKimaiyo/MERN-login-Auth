361adc3259a0597ce24cb05af7e7bc80d5e4e8ef
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-27 11:41:51 +0300

    final commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const  mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const dotenv = require("dotenv");
 
+dotenv.config();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server is Running ${PORT}`)
 })
+
+
+mongoose.connect(process.env.MDB_CONNECT);

62c203deb7bacf918ea7f0100a22292169d7350b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-31 13:43:31 +0300

    final commit

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const userRoutes = require("./routes/users");
-const userRoutes = require("./routes/auth");
+const authRoutes = require("./routes/auth");
 require("dotenv").config();
 
 const cors = require("cors");
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,20 +1,24 @@
 const mongoose = require("mongoose");
-const jwt = reuire("jsonwebtoken");
-const joi = require("joi");
-
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
+
 const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
 });
+
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
-    expiresIn: "7d",
-  });
-  return token;
+	const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+		expiresIn: "7d",
+	});
+	return token;
 };
+
+const User = mongoose.model("user", userSchema);
+
 const validate = (data) => {
 	const schema = Joi.object({
 		firstName: Joi.string().required().label("First Name"),
@@ -26,5 +30,3 @@ const validate = (data) => {
 };
 
 module.exports = { User, validate };
-
-const User = mongoose.model("user", userSchema);

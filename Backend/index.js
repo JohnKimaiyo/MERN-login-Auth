8a5be18ead4412488d440bd26477a0591dad3255
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-31 13:07:30 +0300

    final comit

--- Backend/index.js
+++ Backend/index.js
@@ -2,26 +2,28 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const userRoutes = require("./routes/users");
+const userRoutes = require("./routes/auth");
 require("dotenv").config();
 
 const cors = require("cors");
-const connection = require("./db")
-
+const connection = require("./db");
 
 // connection to db
 connection();
 
-
-app.get('/', (req, res) => {
-  res.send(`<h1>Server is Live </h1>`)
-})
-
+app.get("/", (req, res) => {
+  res.send(`<h1>Server is Live </h1>`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is Running ${PORT}`);
 });
 
-
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// routes
+app.use("api/users", userRoutes);
+app.use("api/auth", authRoutes);
